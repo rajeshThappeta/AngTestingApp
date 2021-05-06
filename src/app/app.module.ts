1f5fa75f4550253da4fa24622ff0bd91a6b38860
rajesh.t7982 <rajesh.t7982@gmail.comm>
2021-05-06 19:55:57 +0530

    components architecture and relationship is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    FooterComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent  {
+
+  products=[1,2,3,4,5,6,7,8]
+
+}
